99cabe1496cfaa59919c3fb88f307b301cb5f328
Katherine0811 <katherine.lee0356@gmail.com>
2024-03-10 13:23:01 +0000

    Getter Setter and Constructor

--- cards/src/main/java/com/tus/cards/dto/ErrorResponseDto.java
+++ cards/src/main/java/com/tus/cards/dto/ErrorResponseDto.java
@@ -8,11 +8,43 @@ import java.time.LocalDateTime;
 @Data @AllArgsConstructor
 
 public class ErrorResponseDto {
-
    
-    private  String apiPath;
+    private String apiPath;
     private HttpStatus errorCode;
-    private  String errorMessage;
+    private String errorMessage;
     private LocalDateTime errorTime;
+    
+	public ErrorResponseDto(String apiPath, HttpStatus errorCode, String errorMessage, LocalDateTime errorTime) {
+		super();
+		this.apiPath = apiPath;
+		this.errorCode = errorCode;
+		this.errorMessage = errorMessage;
+		this.errorTime = errorTime;
+	}
+	
+	public String getApiPath() {
+		return apiPath;
+	}
+	public void setApiPath(String apiPath) {
+		this.apiPath = apiPath;
+	}
+	public HttpStatus getErrorCode() {
+		return errorCode;
+	}
+	public void setErrorCode(HttpStatus errorCode) {
+		this.errorCode = errorCode;
+	}
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+	public void setErrorMessage(String errorMessage) {
+		this.errorMessage = errorMessage;
+	}
+	public LocalDateTime getErrorTime() {
+		return errorTime;
+	}
+	public void setErrorTime(LocalDateTime errorTime) {
+		this.errorTime = errorTime;
+	}
 
 }
--- cards/src/main/java/com/tus/cards/dto/ResponseDto.java
+++ cards/src/main/java/com/tus/cards/dto/ResponseDto.java
@@ -5,9 +5,30 @@ import lombok.Data;
 
 @Data @AllArgsConstructor
 public class ResponseDto {
-
     
     private String statusCode;
     private String statusMsg;
+    
+	public ResponseDto(String statusCode, String statusMsg) {
+		super();
+		this.statusCode = statusCode;
+		this.statusMsg = statusMsg;
+	}
+
+	public String getStatusCode() {
+		return statusCode;
+	}
+
+	public void setStatusCode(String statusCode) {
+		this.statusCode = statusCode;
+	}
+
+	public String getStatusMsg() {
+		return statusMsg;
+	}
+
+	public void setStatusMsg(String statusMsg) {
+		this.statusMsg = statusMsg;
+	}
     
 }
